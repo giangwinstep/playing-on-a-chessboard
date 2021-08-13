33fe6b9854493d6bdc440e31413edd2e4920f255
gianglht <gianglht@outlook.com.vn>
2021-08-14 00:56:52 +0700

    Remove numpy library, reformat code

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-import numpy as np
-from fractions import Fraction
+import fractions
+
 
 def input_n():
     """Input number row column"""
     a = int(input("Please input number of row and column (n x n): "))
     return a
 
+
 def fraction(a, b):
     """Fraction function"""
-    return Fraction(a, b)
+    return fractions.Fraction(a, b)
 
-def printFrac(list1):
+
+def print_frac(list1):
     """Print Fraction"""
     for i in range(len(list1)):
         for j in range(len(list1)):
             print(list1[i][j], end=" ")
         print('\n')
 
-def sumFrac(list1):
+
+def sum_frac(list1):
     """Sum Fraction"""
     sum_val = 0
     for item in list1:
         sum_val += sum(item)
     return sum_val
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     n = input_n()  # number row, column
     template = [i for i in range(1, n + 1)]
     value = []
@@ -34,10 +38,5 @@ if __name__=='__main__':
         temp2 = map(fraction, template, temp1)
         value.append(list(temp2))
 
-    printFrac(value)
-    print(f"Where n = {n} the amount won or lost is: {sumFrac(value)}")
-
-
-
-
-
+    print_frac(value)
+    print(f"Where n = {n} the amount won or lost is: {sum_frac(value)}")
